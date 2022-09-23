06f232bd3205f346cacc2c223ceca5fa79de5a8c
george-luiz <geluiz275@gmail.com>
2022-09-23 10:15:13 -0300

    Primeiro commit: Adicionando projeto robotrom.

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+const controle = document.querySelectorAll("[data-controller]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach(element => {
+    element.addEventListener("click", (event) => {
+        manipulaDados(event.target.textContent, event.target.parentNode);
+        atualizaEstatiticas(event.target.dataset.pecas);
+    });
+});
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function atualizaEstatiticas(peca) {
+    estatistica.forEach(element => {
+        element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica];
+    });
+}
